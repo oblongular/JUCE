oblongular/JUCE

commit 7d1918b385c600e1910dd5b744b583a42a26d8a1

macOS/iOS:  Replace compile-time deployment target checks with runtime checks using the available keyword

modules/juce_audio_processors/format_types/juce_AudioUnitPluginFormat.mm
@@ -39,18 +39,7 @@ JUCE_BEGIN_IGNORE_WARNINGS_GCC_LIKE ("-Wdeprecated-declarations")
  #include <AudioUnit/AudioUnitCarbonView.h>
 #endif
 
-#ifndef JUCE_SUPPORTS_AUv3
- #if __OBJC2__ \
-      && (JUCE_IOS || (defined (MAC_OS_X_VERSION_10_11) && (MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_11)))
-  #define JUCE_SUPPORTS_AUv3 1
- #else
-  #define JUCE_SUPPORTS_AUv3 0
- #endif
-#endif
-
-#if JUCE_SUPPORTS_AUv3
- #include <CoreAudioKit/AUViewController.h>
-#endif
+#include <CoreAudioKit/AUViewController.h>
 
 #include <juce_audio_basics/native/juce_mac_CoreAudioLayouts.h>
 #include <juce_audio_basics/midi/juce_MidiDataConcatenator.h>
@@ -547,9 +536,7 @@ public:
 
         AudioComponentGetDescription (auComponent, &componentDesc);
 
-      #if JUCE_SUPPORTS_AUv3
         isAUv3 = ((componentDesc.componentFlags & kAudioComponentFlag_IsV3AudioUnit) != 0);
-      #endif
 
         wantsMidiMessages = componentDesc.componentType == kAudioUnitType_MusicDevice
                          || componentDesc.componentType == kAudioUnitType_MusicEffect
@@ -1136,18 +1123,12 @@ public:
 
     bool hasEditor() const override
     {
-       #if JUCE_MAC
-        return true;
-       #elif JUCE_SUPPORTS_AUv3
         UInt32 dataSize;
         Boolean isWritable;
 
         return (AudioUnitGetPropertyInfo (audioUnit, kAudioUnitProperty_RequestViewController,
                                           kAudioUnitScope_Global, 0, &dataSize, &isWritable) == noErr
                 && dataSize == sizeof (uintptr_t) && isWritable != 0);
-       #else
-        return false;
-       #endif
     }
 
     AudioProcessorEditor* createEditor() override;
@@ -2261,10 +2242,8 @@ public:
     {
         addAndMakeVisible (wrapper);
 
-       #if JUCE_SUPPORTS_AUv3
         viewControllerCallback =
             CreateObjCBlock (this, &AudioUnitPluginWindowCocoa::requestViewControllerCallback);
-       #endif
 
         setOpaque (true);
         setVisible (true);
@@ -2284,7 +2263,6 @@ public:
         }
     }
 
-   #if JUCE_SUPPORTS_AUv3
     void embedViewController (JUCE_IOS_MAC_VIEW* pluginView, const CGSize& size)
     {
         wrapper.setView (pluginView);
@@ -2299,7 +2277,6 @@ public:
         wrapper.setSize (static_cast<int> (size.width), static_cast<int> (size.height));
       #endif
     }
-   #endif
 
     bool isValid() const        { return wrapper.getView() != nil || waitingForViewCallback; }
 
@@ -2323,10 +2300,8 @@ private:
     AudioUnitPluginInstance& plugin;
     AudioUnitFormatHelpers::AutoResizingNSViewComponent wrapper;
 
-   #if JUCE_SUPPORTS_AUv3
     typedef void (^ViewControllerCallbackBlock)(AUViewControllerBase *);
     ObjCBlock<ViewControllerCallbackBlock> viewControllerCallback;
-   #endif
 
     bool waitingForViewCallback = false;
 
@@ -2375,7 +2350,6 @@ private:
         dataSize = 0;
         isWritable = false;
 
-       #if JUCE_SUPPORTS_AUv3
         if (AudioUnitGetPropertyInfo (plugin.audioUnit, kAudioUnitProperty_RequestViewController, kAudioUnitScope_Global,
                                           0, &dataSize, &isWritable) == noErr
                 && dataSize == sizeof (ViewControllerCallbackBlock))
@@ -2391,7 +2365,6 @@ private:
 
             waitingForViewCallback = false;
         }
-       #endif
 
        #if JUCE_MAC
         if (createGenericViewIfNeeded && (pluginView == nil))
@@ -2418,7 +2391,6 @@ private:
         return pluginView != nil;
     }
 
-   #if JUCE_SUPPORTS_AUv3
     void requestViewControllerCallback (AUViewControllerBase* controller)
     {
         const auto viewSize = [&controller]
@@ -2459,7 +2431,6 @@ private:
             embedViewController ([controller view], viewSize);
         }
     }
-   #endif
 };
 
 #if JUCE_SUPPORT_CARBON
@@ -2693,23 +2664,17 @@ void AudioUnitPluginFormat::createPluginInstance (const PluginDescription& desc,
 
         struct AUAsyncInitializationCallback
         {
-           #if JUCE_SUPPORTS_AUv3
             typedef void (^AUCompletionCallbackBlock)(AudioComponentInstance, OSStatus);
-           #endif
 
             AUAsyncInitializationCallback (double inSampleRate, int inFramesPerBuffer,
                                            PluginCreationCallback inOriginalCallback)
                 : sampleRate (inSampleRate), framesPerBuffer (inFramesPerBuffer),
                   originalCallback (std::move (inOriginalCallback))
             {
-               #if JUCE_SUPPORTS_AUv3
                 block = CreateObjCBlock (this, &AUAsyncInitializationCallback::completion);
-               #endif
             }
 
-           #if JUCE_SUPPORTS_AUv3
-            AUCompletionCallbackBlock getBlock() noexcept       { return block; }
-           #endif
+            AUCompletionCallbackBlock getBlock() noexcept  { return block; }
 
             void completion (AudioComponentInstance audioUnit, OSStatus err)
             {
@@ -2734,26 +2699,18 @@ void AudioUnitPluginFormat::createPluginInstance (const PluginDescription& desc,
             double sampleRate;
             int framesPerBuffer;
             PluginCreationCallback originalCallback;
-
-           #if JUCE_SUPPORTS_AUv3
             ObjCBlock<AUCompletionCallbackBlock> block;
-           #endif
         };
 
         auto callbackBlock = new AUAsyncInitializationCallback (rate, blockSize, std::move (callback));
 
-       #if JUCE_SUPPORTS_AUv3
-        //==============================================================================
-        bool isAUv3 = ((componentDesc.componentFlags & kAudioComponentFlag_IsV3AudioUnit) != 0);
-
-        if (isAUv3)
+        if ((componentDesc.componentFlags & kAudioComponentFlag_IsV3AudioUnit) != 0)
         {
             AudioComponentInstantiate (auComponent, kAudioComponentInstantiation_LoadOutOfProcess,
                                        callbackBlock->getBlock());
 
             return;
         }
-       #endif // JUCE_SUPPORTS_AUv3
 
         AudioComponentInstance audioUnit;
         auto err = AudioComponentInstanceNew(auComponent, &audioUnit);
@@ -2767,7 +2724,6 @@ void AudioUnitPluginFormat::createPluginInstance (const PluginDescription& desc,
 
 bool AudioUnitPluginFormat::requiresUnblockedMessageThreadDuringCreation (const PluginDescription& desc) const
 {
-   #if JUCE_SUPPORTS_AUv3
     String pluginName, version, manufacturer;
     AudioComponentDescription componentDesc;
 
@@ -2780,9 +2736,6 @@ bool AudioUnitPluginFormat::requiresUnblockedMessageThreadDuringCreation (const 
             if (AudioComponentGetDescription (auComp, &componentDesc) == noErr)
                 return ((componentDesc.componentFlags & kAudioComponentFlag_IsV3AudioUnit) != 0);
     }
-   #else
-    ignoreUnused (desc);
-   #endif
 
     return false;
 }
@@ -2815,11 +2768,9 @@ StringArray AudioUnitPluginFormat::searchPathsForPlugins (const FileSearchPath&,
         {
             ignoreUnused (allowPluginsWhichRequireAsynchronousInstantiation);
 
-           #if JUCE_SUPPORTS_AUv3
-            bool isAUv3 = ((desc.componentFlags & kAudioComponentFlag_IsV3AudioUnit) != 0);
+            const auto isAUv3 = ((desc.componentFlags & kAudioComponentFlag_IsV3AudioUnit) != 0);
 
             if (allowPluginsWhichRequireAsynchronousInstantiation || ! isAUv3)
-           #endif
                 result.add (AudioUnitFormatHelpers::createPluginIdentifier (desc));
         }
     }

modules/juce_audio_devices/native/juce_ios_Audio.cpp
@@ -722,11 +722,20 @@ struct iOSAudioIODevice::Pimpl      : public AudioPlayHead,
                                             &dataSize);
         if (err == noErr)
         {
-           #if (! defined __IPHONE_10_0) || (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)
-            [[UIApplication sharedApplication] openURL: (NSURL*)hostUrl];
-           #else
-            [[UIApplication sharedApplication] openURL: (NSURL*)hostUrl options: @{} completionHandler: nil];
+           #if defined (__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+            if (@available (iOS 10.0, *))
+            {
+                [[UIApplication sharedApplication] openURL: (NSURL*) hostUrl
+                                                   options: @{}
+                                         completionHandler: nil];
+
+                return;
+            }
            #endif
+
+            JUCE_BEGIN_IGNORE_WARNINGS_GCC_LIKE ("-Wdeprecated-declarations")
+            [[UIApplication sharedApplication] openURL: (NSURL*) hostUrl];
+            JUCE_END_IGNORE_WARNINGS_GCC_LIKE
         }
     }
 

modules/juce_audio_devices/native/juce_mac_CoreMidi.mm
@@ -82,7 +82,7 @@ namespace CoreMidiHelpers
     struct Sender;
 
    #if JUCE_HAS_NEW_COREMIDI_API
-    JUCE_BEGIN_IGNORE_WARNINGS_GCC_LIKE ("-Wunguarded-availability-new")
+    JUCE_BEGIN_IGNORE_WARNINGS_GCC_LIKE ("-Wunguarded-availability", "-Wunguarded-availability-new")
 
     template <>
     struct Sender<ImplementationStrategy::onlyNew> : public SenderBase
@@ -829,7 +829,7 @@ namespace CoreMidiHelpers
     struct CreatorFunctions;
 
    #if JUCE_HAS_NEW_COREMIDI_API
-    JUCE_BEGIN_IGNORE_WARNINGS_GCC_LIKE ("-Wunguarded-availability-new")
+    JUCE_BEGIN_IGNORE_WARNINGS_GCC_LIKE ("-Wunguarded-availability", "-Wunguarded-availability-new")
 
     template <>
     struct CreatorFunctions<ImplementationStrategy::onlyNew>

modules/juce_audio_plugin_client/AU/juce_AUv3_Wrapper.mm
@@ -28,27 +28,19 @@
 
 #if JucePlugin_Build_AUv3
 
-#if JUCE_MAC
- #if (! defined MAC_OS_X_VERSION_MIN_REQUIRED) || (! defined MAC_OS_X_VERSION_10_11) || (MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_11)
-  #error AUv3 needs Deployment Target OS X 10.11 or higher to compile
- #endif
- #if (defined MAC_OS_X_VERSION_10_13) && (MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_13)
-  #define JUCE_AUV3_MIDI_OUTPUT_SUPPORTED 1
-  #define JUCE_AUV3_VIEW_CONFIG_SUPPORTED 1
- #endif
- #if defined (MAC_OS_VERSION_12_0)
-  #define JUCE_AUV3_MIDI_EVENT_LIST_SUPPORTED 1
- #endif
+#if JUCE_MAC && ! (defined (MAC_OS_X_VERSION_10_11) && MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_11)
+ #error AUv3 needs Deployment Target OS X 10.11 or higher to compile
 #endif
 
-#if JUCE_IOS
- #if (defined __IPHONE_11_0) && (__IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_11_0)
-  #define JUCE_AUV3_MIDI_OUTPUT_SUPPORTED 1
-  #define JUCE_AUV3_VIEW_CONFIG_SUPPORTED 1
- #endif
- #if (defined __IPHONE_15_0)
-  #define JUCE_AUV3_MIDI_EVENT_LIST_SUPPORTED 1
- #endif
+#if (JUCE_IOS && defined (__IPHONE_15_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_15_0) \
+   || (JUCE_MAC && defined (MAC_OS_VERSION_12_0) && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_VERSION_12_0)
+ #define JUCE_AUV3_MIDI_EVENT_LIST_SUPPORTED 1
+#endif
+
+#if (JUCE_IOS && defined (__IPHONE_11_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_11_0) \
+   || (JUCE_MAC && defined (MAC_OS_X_VERSION_10_13) && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_13)
+ #define JUCE_AUV3_MIDI_OUTPUT_SUPPORTED 1
+ #define JUCE_AUV3_VIEW_CONFIG_SUPPORTED 1
 #endif
 
 #ifndef __OBJC2__
@@ -210,8 +202,10 @@ public:
 
     //==============================================================================
    #if JUCE_AUV3_VIEW_CONFIG_SUPPORTED
+    JUCE_BEGIN_IGNORE_WARNINGS_GCC_LIKE ("-Wunguarded-availability", "-Wunguarded-availability-new")
     virtual NSIndexSet* getSupportedViewConfigurations (NSArray<AUAudioUnitViewConfiguration*>*) = 0;
-    virtual void selectViewConfiguration (AUAudioUnitViewConfiguration*)   = 0;
+    virtual void selectViewConfiguration (AUAudioUnitViewConfiguration*) = 0;
+    JUCE_END_IGNORE_WARNINGS_GCC_LIKE
    #endif
 
 private:
@@ -265,7 +259,8 @@ private:
             JUCE_END_IGNORE_WARNINGS_GCC_LIKE
 
            #if JUCE_AUV3_MIDI_OUTPUT_SUPPORTED
-            addMethod (@selector (MIDIOutputNames),                 getMIDIOutputNames,             "@@:");
+            if (@available (macOS 10.13, iOS 11.0, *))
+                addMethod (@selector (MIDIOutputNames), getMIDIOutputNames, "@@:");
            #endif
 
             //==============================================================================
@@ -284,8 +279,11 @@ private:
 
             //==============================================================================
            #if JUCE_AUV3_VIEW_CONFIG_SUPPORTED
-            addMethod (@selector (supportedViewConfigurations:),    getSupportedViewConfigurations, "@@:@");
-            addMethod (@selector (selectViewConfiguration:),        selectViewConfiguration,        "v@:@");
+            if (@available (macOS 10.13, iOS 11.0, *))
+            {
+                addMethod (@selector (supportedViewConfigurations:),    getSupportedViewConfigurations, "@@:@");
+                addMethod (@selector (selectViewConfiguration:),        selectViewConfiguration,        "v@:@");
+            }
            #endif
 
             registerClass();
@@ -396,8 +394,10 @@ private:
 
         //==============================================================================
        #if JUCE_AUV3_VIEW_CONFIG_SUPPORTED
+        JUCE_BEGIN_IGNORE_WARNINGS_GCC_LIKE ("-Wunguarded-availability", "-Wunguarded-availability-new")
         static NSIndexSet* getSupportedViewConfigurations (id self, SEL, NSArray<AUAudioUnitViewConfiguration*>* configs) { return _this (self)->getSupportedViewConfigurations (configs); }
-        static void selectViewConfiguration (id self, SEL, AUAudioUnitViewConfiguration* config)    { _this (self)->selectViewConfiguration (config); }
+        static void selectViewConfiguration (id self, SEL, AUAudioUnitViewConfiguration* config)                          { _this (self)->selectViewConfiguration (config); }
+        JUCE_END_IGNORE_WARNINGS_GCC_LIKE
        #endif
     };
 
@@ -896,6 +896,7 @@ public:
 
     //==============================================================================
    #if JUCE_AUV3_VIEW_CONFIG_SUPPORTED
+    JUCE_BEGIN_IGNORE_WARNINGS_GCC_LIKE ("-Wunguarded-availability", "-Wunguarded-availability-new")
     NSIndexSet* getSupportedViewConfigurations (NSArray<AUAudioUnitViewConfiguration*>* configs) override
     {
         auto supportedViewIndecies = [[NSMutableIndexSet alloc] init];
@@ -932,6 +933,7 @@ public:
     {
         processorHolder->viewConfiguration.reset (new AudioProcessorHolder::ViewConfig { [config width], [config height], [config hostHasController] == YES });
     }
+    JUCE_END_IGNORE_WARNINGS_GCC_LIKE
    #endif
 
     struct ScopedKeyChange
@@ -1598,10 +1600,11 @@ private:
 
             // send MIDI
            #if JucePlugin_ProducesMidiOutput && JUCE_AUV3_MIDI_OUTPUT_SUPPORTED
-            if (auto midiOut = [au MIDIOutputEventBlock])
+            if (@available (macOS 10.13, iOS 11.0, *))
             {
-                for (const auto metadata : midiMessages)
-                    midiOut (metadata.samplePosition, 0, metadata.numBytes, metadata.data);
+                if (auto midiOut = [au MIDIOutputEventBlock])
+                    for (const auto metadata : midiMessages)
+                        midiOut (metadata.samplePosition, 0, metadata.numBytes, metadata.data);
             }
            #endif
 

modules/juce_audio_utils/native/juce_mac_BluetoothMidiDevicePairingDialogue.mm
@@ -26,8 +26,6 @@
 namespace juce
 {
 
-#if defined (MAC_OS_X_VERSION_10_11) && MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_11
-
 //==============================================================================
 class BluetoothMidiPairingWindowClass   : public ObjCClass<NSObject>
 {
@@ -159,20 +157,12 @@ private:
 bool BluetoothMidiDevicePairingDialogue::open (ModalComponentManager::Callback* exitCallback,
                                                Rectangle<int>* bounds)
 {
-    new BluetoothMidiSelectorWindowHelper (exitCallback, bounds);
-    return true;
-}
+    if (@available (macOS 10.11, *))
+    {
+        new BluetoothMidiSelectorWindowHelper (exitCallback, bounds);
+        return true;
+    }
 
-bool BluetoothMidiDevicePairingDialogue::isAvailable()
-{
-    return true;
-}
-
-#else
-
-bool BluetoothMidiDevicePairingDialogue::open (ModalComponentManager::Callback* exitCallback,
-                                               Rectangle<int>*)
-{
     std::unique_ptr<ModalComponentManager::Callback> cb (exitCallback);
     // This functionality is unavailable when targetting OSX < 10.11. Instead,
     // you should pair Bluetooth MIDI devices using the "Audio MIDI Setup" app
@@ -183,9 +173,10 @@ bool BluetoothMidiDevicePairingDialogue::open (ModalComponentManager::Callback* 
 
 bool BluetoothMidiDevicePairingDialogue::isAvailable()
 {
+    if (@available (macOS 10.11, *))
+        return true;
+
     return false;
 }
-
-#endif
 
 } // namespace juce

modules/juce_core/native/juce_BasicNativeHeaders.h
@@ -28,7 +28,7 @@
 #if JUCE_MAC || JUCE_IOS
 
  #if JUCE_IOS
-  #if JUCE_MODULE_AVAILABLE_juce_opengl && defined (__IPHONE_12_0) && __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_12_0
+  #if JUCE_MODULE_AVAILABLE_juce_opengl && defined (__IPHONE_12_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_12_0
    #define GLES_SILENCE_DEPRECATION 1
   #endif
 

modules/juce_core/native/juce_mac_Files.mm
@@ -410,12 +410,20 @@ bool JUCE_CALLTYPE Process::openDocument (const String& fileName, const String& 
       #if JUCE_IOS
         ignoreUnused (parameters);
 
-       #if (! defined __IPHONE_OS_VERSION_MIN_REQUIRED) || (! defined __IPHONE_10_0) || (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)
-        return [[UIApplication sharedApplication] openURL: filenameAsURL];
-       #else
-        [[UIApplication sharedApplication] openURL: filenameAsURL options: @{} completionHandler: nil];
-        return true;
+       #if defined (__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+        if (@available (iOS 10.0, *))
+        {
+            [[UIApplication sharedApplication] openURL: filenameAsURL
+                                               options: @{}
+                                     completionHandler: nil];
+
+            return true;
+        }
        #endif
+
+        JUCE_BEGIN_IGNORE_WARNINGS_GCC_LIKE ("-Wdeprecated-declarations")
+        return [[UIApplication sharedApplication] openURL: filenameAsURL];
+        JUCE_END_IGNORE_WARNINGS_GCC_LIKE
       #else
         NSWorkspace* workspace = [NSWorkspace sharedWorkspace];
 
@@ -434,16 +442,21 @@ bool JUCE_CALLTYPE Process::openDocument (const String& fileName, const String& 
             for (int i = 0; i < params.size(); ++i)
                 [paramArray addObject: juceStringToNS (params[i])];
 
-           #if (defined MAC_OS_X_VERSION_10_15) && MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_15
-            auto config = [NSWorkspaceOpenConfiguration configuration];
-            [config setCreatesNewApplicationInstance: YES];
-            config.arguments = paramArray;
+           #if defined (MAC_OS_X_VERSION_10_15) && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_15
+            if (@available (macOS 10.15, *))
+            {
+                auto config = [NSWorkspaceOpenConfiguration configuration];
+                [config setCreatesNewApplicationInstance: YES];
+                config.arguments = paramArray;
 
-            [workspace openApplicationAtURL: filenameAsURL
-                              configuration: config
-                          completionHandler: nil];
-            return true;
-           #else
+                [workspace openApplicationAtURL: filenameAsURL
+                                  configuration: config
+                              completionHandler: nil];
+
+                return true;
+            }
+           #endif
+
             NSMutableDictionary* dict = [[NSMutableDictionary new] autorelease];
 
             [dict setObject: paramArray
@@ -453,7 +466,6 @@ bool JUCE_CALLTYPE Process::openDocument (const String& fileName, const String& 
                                              options: NSWorkspaceLaunchDefault | NSWorkspaceLaunchNewInstance
                                        configuration: dict
                                                error: nil];
-           #endif
         }
 
         if (file.exists())

commit 1487205386772f18373efcbf48a2aeb5794ec120

AudioUnitPluginFormat:  Fix result of hasEditor on Mac
Fixes a bug introduced in 7d1918b385c600e1910dd5b744b583a42a26d8a1,
where Ozone 9's editor failed to open correctly in the AudioPluginHost.

modules/juce_audio_processors/format_types/juce_AudioUnitPluginFormat.mm
@@ -1123,12 +1123,16 @@ public:
 
     bool hasEditor() const override
     {
+       #if JUCE_MAC
+        return true;
+       #else
         UInt32 dataSize;
         Boolean isWritable;
 
         return (AudioUnitGetPropertyInfo (audioUnit, kAudioUnitProperty_RequestViewController,
                                           kAudioUnitScope_Global, 0, &dataSize, &isWritable) == noErr
                 && dataSize == sizeof (uintptr_t) && isWritable != 0);
+       #endif
     }
 
     AudioProcessorEditor* createEditor() override;