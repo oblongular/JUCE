oblongular/JUCE

commit da38c1ed2ad695659d35f0d9d7956ae449d6ae63

TextInputTarget:  Improve IME support on Android

modules/juce_product_unlocking/native/juce_android_InAppPurchases.cpp
@@ -143,7 +143,7 @@ inline StringArray javaListOfStringToJuceStringArray (const LocalRef<jobject>& j
 }
 
 //==============================================================================
-constexpr unsigned char juceBillingClientCompiled[]
+constexpr uint8 juceBillingClientCompiled[]
 {
     0x1f, 0x8b, 0x08, 0x08, 0xa4, 0x53, 0xd0, 0x62, 0x04, 0x03, 0x63, 0x6c,
     0x61, 0x73, 0x73, 0x65, 0x73, 0x2e, 0x64, 0x65, 0x78, 0x00, 0x9d, 0x5a,
@@ -720,41 +720,16 @@ private:
       METHOD (queryPurchases,                "queryPurchases",              "()V")                                                                        \
       METHOD (consumePurchase,               "consumePurchase",             "(Ljava/lang/String;Ljava/lang/String;)V")                                    \
                                                                                                                                                           \
-      CALLBACK (productDetailsQueryCallback, "productDetailsQueryCallback", "(JLjava/util/List;)V")                                                       \
-      CALLBACK (purchasesListQueryCallback,  "purchasesListQueryCallback",  "(JLjava/util/List;)V")                                                       \
-      CALLBACK (purchaseCompletedCallback,   "purchaseCompletedCallback",   "(JLcom/android/billingclient/api/Purchase;I)V")                              \
-      CALLBACK (purchaseConsumedCallback,    "purchaseConsumedCallback",    "(JLjava/lang/String;I)V")
+      CALLBACK (generatedCallback<&Pimpl::updateProductDetails>, "productDetailsQueryCallback", "(JLjava/util/List;)V")                                                       \
+      CALLBACK (generatedCallback<&Pimpl::updatePurchasesList>,  "purchasesListQueryCallback",  "(JLjava/util/List;)V")                                                       \
+      CALLBACK (generatedCallback<&Pimpl::purchaseCompleted>,    "purchaseCompletedCallback",   "(JLcom/android/billingclient/api/Purchase;I)V")                              \
+      CALLBACK (generatedCallback<&Pimpl::purchaseConsumed>,     "purchaseConsumedCallback",    "(JLjava/lang/String;I)V")
 
     DECLARE_JNI_CLASS_WITH_BYTECODE (JuceBillingClient,
                                      "com/rmsl/juce/JuceBillingClient",
                                      16,
-                                     juceBillingClientCompiled,
-                                     numElementsInArray (juceBillingClientCompiled))
+                                     juceBillingClientCompiled)
     #undef JNI_CLASS_MEMBERS
-
-    static void JNICALL productDetailsQueryCallback (JNIEnv*, jobject, jlong host, jobject productDetailsList)
-    {
-        if (auto* myself = reinterpret_cast<Pimpl*> (host))
-            myself->updateProductDetails (productDetailsList);
-    }
-
-    static void JNICALL purchasesListQueryCallback (JNIEnv*, jobject, jlong host, jobject purchasesList)
-    {
-        if (auto* myself = reinterpret_cast<Pimpl*> (host))
-            myself->updatePurchasesList (purchasesList);
-    }
-
-    static void JNICALL purchaseCompletedCallback (JNIEnv*, jobject, jlong host, jobject purchase, int responseCode)
-    {
-        if (auto* myself = reinterpret_cast<Pimpl*> (host))
-            myself->purchaseCompleted (purchase, responseCode);
-    }
-
-    static void JNICALL purchaseConsumedCallback (JNIEnv*, jobject, jlong host, jstring productIdentifier, int responseCode)
-    {
-        if (auto* myself = reinterpret_cast<Pimpl*> (host))
-            myself->purchaseConsumed (productIdentifier, responseCode);
-    }
 
     //==============================================================================
     bool isReady() const
@@ -1094,8 +1069,5 @@ void juce_handleOnResume()
         InAppPurchases::getInstance()->restoreProductsBoughtList (false);
     });
 }
-
-
-InAppPurchases::Pimpl::JuceBillingClient_Class InAppPurchases::Pimpl::JuceBillingClient;
 
 } // namespace juce

modules/juce_audio_devices/native/juce_android_Midi.cpp
@@ -327,7 +327,7 @@ static const uint8 javaMidiByteCode[] =
  STATICMETHOD (getAndroidMidiDeviceManager, "getAndroidMidiDeviceManager", "(Landroid/content/Context;)Lcom/rmsl/juce/JuceMidiSupport$MidiDeviceManager;") \
  STATICMETHOD (getAndroidBluetoothManager,  "getAndroidBluetoothManager",  "(Landroid/content/Context;)Lcom/rmsl/juce/JuceMidiSupport$BluetoothManager;")
 
-DECLARE_JNI_CLASS_WITH_BYTECODE (JuceMidiSupport, "com/rmsl/juce/JuceMidiSupport", 23, javaMidiByteCode, sizeof (javaMidiByteCode))
+DECLARE_JNI_CLASS_WITH_BYTECODE (JuceMidiSupport, "com/rmsl/juce/JuceMidiSupport", 23, javaMidiByteCode)
 #undef JNI_CLASS_MEMBERS
 
 #define JNI_CLASS_MEMBERS(METHOD, STATICMETHOD, FIELD, STATICFIELD, CALLBACK) \

modules/juce_core/misc/juce_Functional.h
@@ -113,4 +113,19 @@ Object withMember (Object copy, Member OtherObject::* member, Other&& value)
 template <typename Fn> struct ScopeGuard : Fn { ~ScopeGuard() { Fn::operator()(); } };
 template <typename Fn> ScopeGuard (Fn) -> ScopeGuard<Fn>;
 
+#ifndef DOXYGEN
+namespace detail
+{
+template <typename Functor, typename Return, typename... Args>
+static constexpr auto toFnPtr (Functor functor, Return (Functor::*) (Args...) const)
+{
+    return static_cast<Return (*) (Args...)> (functor);
+}
+} // namespace detail
+#endif
+
+/** Converts a captureless lambda to its equivalent function pointer type. */
+template <typename Functor>
+static constexpr auto toFnPtr (Functor functor) { return detail::toFnPtr (functor, &Functor::operator()); }
+
 } // namespace juce

modules/juce_core/native/juce_android_JNIHelpers.cpp
@@ -62,7 +62,7 @@ static const uint8 invocationHandleByteCode[] =
  CALLBACK (juce_invokeImplementer, "dispatchInvoke", "(JLjava/lang/Object;Ljava/lang/reflect/Method;[Ljava/lang/Object;)Ljava/lang/Object;") \
  CALLBACK (juce_dispatchDelete, "dispatchFinalize", "(J)V")
 
- DECLARE_JNI_CLASS_WITH_BYTECODE (JuceInvocationHandler, "com/rmsl/juce/JuceInvocationHandler", 10, invocationHandleByteCode, sizeof (invocationHandleByteCode))
+ DECLARE_JNI_CLASS_WITH_BYTECODE (JuceInvocationHandler, "com/rmsl/juce/JuceInvocationHandler", 10, invocationHandleByteCode)
 #undef JNI_CLASS_MEMBERS
 
 #define JNI_CLASS_MEMBERS(METHOD, STATICMETHOD, FIELD, STATICFIELD, CALLBACK) \
@@ -116,7 +116,7 @@ struct SystemJavaClassComparator
 };
 
 //==============================================================================
-JNIClassBase::JNIClassBase (const char* cp, int classMinSDK, const void* bc, size_t n)
+JNIClassBase::JNIClassBase (const char* cp, int classMinSDK, const uint8* bc, size_t n)
     : classPath (cp), byteCode (bc), byteCodeSize (n), minSDK (classMinSDK), classRef (nullptr)
 {
     SystemJavaClassComparator comparator;
@@ -552,12 +552,12 @@ static const uint8 javaFragmentOverlay[] =
 #define JNI_CLASS_MEMBERS(METHOD, STATICMETHOD, FIELD, STATICFIELD, CALLBACK) \
  METHOD (construct,   "<init>",   "()V") \
  METHOD (close,       "close",    "()V") \
- CALLBACK (FragmentOverlay::onActivityResultNative, "onActivityResultNative", "(JIILandroid/content/Intent;)V") \
- CALLBACK (FragmentOverlay::onCreateNative,         "onCreateNative",         "(JLandroid/os/Bundle;)V") \
- CALLBACK (FragmentOverlay::onStartNative,          "onStartNative",          "(J)V") \
- CALLBACK (FragmentOverlay::onRequestPermissionsResultNative, "onRequestPermissionsResultNative", "(JI[Ljava/lang/String;[I)V")
+ CALLBACK (generatedCallback<&FragmentOverlay::onActivityResultCallback>,           "onActivityResultNative",           "(JIILandroid/content/Intent;)V") \
+ CALLBACK (generatedCallback<&FragmentOverlay::onCreatedCallback>,                  "onCreateNative",                   "(JLandroid/os/Bundle;)V") \
+ CALLBACK (generatedCallback<&FragmentOverlay::onStartCallback>,                    "onStartNative",                    "(J)V") \
+ CALLBACK (generatedCallback<&FragmentOverlay::onRequestPermissionsResultCallback>, "onRequestPermissionsResultNative", "(JI[Ljava/lang/String;[I)V")
 
- DECLARE_JNI_CLASS_WITH_BYTECODE (JuceFragmentOverlay, "com/rmsl/juce/FragmentOverlay", 16, javaFragmentOverlay, sizeof(javaFragmentOverlay))
+ DECLARE_JNI_CLASS_WITH_BYTECODE (JuceFragmentOverlay, "com/rmsl/juce/FragmentOverlay", 16, javaFragmentOverlay)
 #undef JNI_CLASS_MEMBERS
 
 #define JNI_CLASS_MEMBERS(METHOD, STATICMETHOD, FIELD, STATICFIELD, CALLBACK) \
@@ -590,47 +590,39 @@ void FragmentOverlay::open()
     env->CallVoidMethod (native.get(), AndroidDialogFragment.show, fm.get(), javaString ("FragmentOverlay").get());
 }
 
-void FragmentOverlay::onActivityResultNative (JNIEnv* env, jobject, jlong host,
-                                              jint requestCode, jint resultCode, jobject data)
+void FragmentOverlay::onCreatedCallback (JNIEnv* env, FragmentOverlay& t, jobject obj)
 {
-    if (auto* myself = reinterpret_cast<FragmentOverlay*> (host))
-        myself->onActivityResult (requestCode, resultCode, LocalRef<jobject> (env->NewLocalRef (data)));
+    t.onCreated (LocalRef<jobject> { env->NewLocalRef (obj) });
 }
 
-void FragmentOverlay::onCreateNative (JNIEnv* env, jobject, jlong host, jobject bundle)
+void FragmentOverlay::onStartCallback (JNIEnv*, FragmentOverlay& t)
 {
-    if (auto* myself = reinterpret_cast<FragmentOverlay*> (host))
-        myself->onCreated (LocalRef<jobject> (env->NewLocalRef (bundle)));
+    t.onStart();
 }
 
-void FragmentOverlay::onStartNative (JNIEnv*, jobject, jlong host)
+void FragmentOverlay::onRequestPermissionsResultCallback (JNIEnv* env, FragmentOverlay& t, jint requestCode, jobjectArray jPermissions, jintArray jGrantResults)
 {
-    if (auto* myself = reinterpret_cast<FragmentOverlay*> (host))
-        myself->onStart();
-}
+    Array<int> grantResults;
+    int n = (jGrantResults != nullptr ? env->GetArrayLength (jGrantResults) : 0);
 
-void FragmentOverlay::onRequestPermissionsResultNative (JNIEnv* env, jobject, jlong host, jint requestCode,
-                                                        jobjectArray jPermissions, jintArray jGrantResults)
-{
-    if (auto* myself = reinterpret_cast<FragmentOverlay*> (host))
+    if (n > 0)
     {
-        Array<int> grantResults;
-        int n = (jGrantResults != nullptr ? env->GetArrayLength (jGrantResults) : 0);
+        auto* data = env->GetIntArrayElements (jGrantResults, nullptr);
 
-        if (n > 0)
-        {
-            auto* data = env->GetIntArrayElements (jGrantResults, nullptr);
+        for (int i = 0; i < n; ++i)
+            grantResults.add (data[i]);
 
-            for (int i = 0; i < n; ++i)
-                grantResults.add (data[i]);
-
-            env->ReleaseIntArrayElements (jGrantResults, data, 0);
-        }
-
-        myself->onRequestPermissionsResult (requestCode,
-                                            javaStringArrayToJuce (LocalRef<jobjectArray> (jPermissions)),
-                                            grantResults);
+        env->ReleaseIntArrayElements (jGrantResults, data, 0);
     }
+
+    t.onRequestPermissionsResult (requestCode,
+                                  javaStringArrayToJuce (LocalRef<jobjectArray> (jPermissions)),
+                                  grantResults);
+}
+
+void FragmentOverlay::onActivityResultCallback (JNIEnv* env, FragmentOverlay& t, jint requestCode, jint resultCode, jobject data)
+{
+    t.onActivityResult (requestCode, resultCode, LocalRef<jobject> (env->NewLocalRef (data)));
 }
 
 jobject FragmentOverlay::getNativeHandle()

modules/juce_core/native/juce_android_JNIHelpers.h
@@ -172,7 +172,7 @@ struct SystemJavaClassComparator;
 class JNIClassBase
 {
 public:
-    JNIClassBase (const char* classPath, int minSDK, const void* byteCode, size_t byteCodeSize);
+    JNIClassBase (const char* classPath, int minSDK, const uint8* byteCode, size_t byteCodeSize);
     virtual ~JNIClassBase();
 
     operator jclass() const noexcept  { return classRef; }
@@ -210,6 +210,9 @@ private:
 };
 
 //==============================================================================
+template <typename T, size_t N> constexpr auto numBytes (const T (&) [N]) { return N; }
+                                constexpr auto numBytes (std::nullptr_t)  { return static_cast<size_t> (0); }
+
 #define CREATE_JNI_METHOD(methodID, stringName, params)          methodID = resolveMethod (env, stringName, params);
 #define CREATE_JNI_STATICMETHOD(methodID, stringName, params)    methodID = resolveStaticMethod (env, stringName, params);
 #define CREATE_JNI_FIELD(fieldID, stringName, signature)         fieldID  = resolveField (env, stringName, signature);
@@ -219,26 +222,26 @@ private:
 #define DECLARE_JNI_FIELD(fieldID, stringName, signature)        jfieldID  fieldID;
 #define DECLARE_JNI_CALLBACK(fieldID, stringName, signature)
 
-#define DECLARE_JNI_CLASS_WITH_BYTECODE(CppClassName, javaPath, minSDK, byteCodeData, byteCodeSize) \
-    class CppClassName ## _Class   : public JNIClassBase \
-    { \
-    public: \
-        CppClassName ## _Class() : JNIClassBase (javaPath, minSDK, byteCodeData, byteCodeSize) {} \
-    \
-        void initialiseFields (JNIEnv* env) \
-        { \
-            Array<JNINativeMethod> callbacks; \
-            JNI_CLASS_MEMBERS (CREATE_JNI_METHOD, CREATE_JNI_STATICMETHOD, CREATE_JNI_FIELD, CREATE_JNI_STATICFIELD, CREATE_JNI_CALLBACK); \
-            resolveCallbacks (env, callbacks); \
-        } \
-    \
-        JNI_CLASS_MEMBERS (DECLARE_JNI_METHOD, DECLARE_JNI_METHOD, DECLARE_JNI_FIELD, DECLARE_JNI_FIELD, DECLARE_JNI_CALLBACK) \
-    }; \
-    static CppClassName ## _Class CppClassName;
+#define DECLARE_JNI_CLASS_WITH_BYTECODE(CppClassName, javaPath, minSDK, byteCodeData)                                                       \
+    class CppClassName ## _Class   : public JNIClassBase                                                                                    \
+    {                                                                                                                                       \
+    public:                                                                                                                                 \
+        CppClassName ## _Class() : JNIClassBase (javaPath, minSDK, byteCodeData, numBytes (byteCodeData)) {}                                \
+                                                                                                                                            \
+        void initialiseFields (JNIEnv* env)                                                                                                 \
+        {                                                                                                                                   \
+            Array<JNINativeMethod> callbacks;                                                                                               \
+            JNI_CLASS_MEMBERS (CREATE_JNI_METHOD, CREATE_JNI_STATICMETHOD, CREATE_JNI_FIELD, CREATE_JNI_STATICFIELD, CREATE_JNI_CALLBACK);  \
+            resolveCallbacks (env, callbacks);                                                                                              \
+        }                                                                                                                                   \
+                                                                                                                                            \
+        JNI_CLASS_MEMBERS (DECLARE_JNI_METHOD, DECLARE_JNI_METHOD, DECLARE_JNI_FIELD, DECLARE_JNI_FIELD, DECLARE_JNI_CALLBACK)              \
+    };                                                                                                                                      \
+    static inline const CppClassName ## _Class CppClassName;
 
 //==============================================================================
 #define DECLARE_JNI_CLASS_WITH_MIN_SDK(CppClassName, javaPath, minSDK) \
-    DECLARE_JNI_CLASS_WITH_BYTECODE (CppClassName, javaPath, minSDK, nullptr, 0)
+    DECLARE_JNI_CLASS_WITH_BYTECODE (CppClassName, javaPath, minSDK, nullptr)
 
 //==============================================================================
 #define DECLARE_JNI_CLASS(CppClassName, javaPath) \
@@ -1005,20 +1008,20 @@ public:
                                              const Array<int>& /*grantResults*/) {}
     virtual void onActivityResult (int /*requestCode*/, int /*resultCode*/, LocalRef<jobject> /*data*/) {}
 
+    /** @internal */
+    static void onCreatedCallback (JNIEnv*, FragmentOverlay&, jobject obj);
+    /** @internal */
+    static void onStartCallback (JNIEnv*, FragmentOverlay&);
+    /** @internal */
+    static void onRequestPermissionsResultCallback (JNIEnv*, FragmentOverlay&, jint requestCode, jobjectArray jPermissions, jintArray jGrantResults);
+    /** @internal */
+    static void onActivityResultCallback (JNIEnv*, FragmentOverlay&, jint requestCode, jint resultCode, jobject data);
+
 protected:
     jobject getNativeHandle();
 
 private:
-
     GlobalRef native;
-
-public:
-    /* internal: do not use */
-    static void onActivityResultNative (JNIEnv*, jobject, jlong, jint, jint, jobject);
-    static void onCreateNative (JNIEnv*, jobject, jlong, jobject);
-    static void onStartNative (JNIEnv*, jobject, jlong);
-    static void onRequestPermissionsResultNative (JNIEnv*, jobject, jlong, jint,
-                                                  jobjectArray, jintArray);
 };
 
 //==============================================================================
@@ -1029,5 +1032,31 @@ void startAndroidActivityForResult (const LocalRef<jobject>& intent, int request
 //==============================================================================
 bool androidHasSystemFeature (const String& property);
 String audioManagerGetProperty (const String& property);
+
+namespace detail
+{
+
+template <auto Fn, typename Result, typename Class, typename... Args>
+inline constexpr auto generatedCallbackImpl =
+    juce::toFnPtr (JNICALL [] (JNIEnv* env, jobject, jlong host, Args... args) -> Result
+                   {
+                       if (auto* object = reinterpret_cast<Class*> (host))
+                           return Fn (env, *object, args...);
+
+                       return {};
+                   });
+
+template <auto Fn, typename Result, typename Class, typename... Args>
+constexpr auto generateCallbackImpl (Result (*) (JNIEnv*, Class&, Args...))        { return generatedCallbackImpl<Fn, Result, Class, Args...>; }
+
+template <auto Fn, typename Result, typename Class, typename... Args>
+constexpr auto generateCallbackImpl (Result (*) (JNIEnv*, const Class&, Args...))  { return generatedCallbackImpl<Fn, Result, Class, Args...>; }
+
+} // namespace detail
+
+// Evaluates to a static function that forwards to the provided Fn, assuming that the
+// 'host' argument points to an object on which it is valid to call Fn
+template <auto Fn>
+inline constexpr auto generatedCallback = detail::generateCallbackImpl<Fn> (Fn);
 
 } // namespace juce

modules/juce_core/native/juce_android_Network.cpp
@@ -195,7 +195,7 @@ DECLARE_JNI_CLASS (StringBuffer, "java/lang/StringBuffer")
  METHOD (isExhausted, "isExhausted", "()Z") \
  METHOD (setPosition, "setPosition", "(J)Z") \
 
-DECLARE_JNI_CLASS_WITH_BYTECODE (HTTPStream, "com/rmsl/juce/JuceHTTPStream", 16, javaJuceHttpStream, sizeof(javaJuceHttpStream))
+DECLARE_JNI_CLASS_WITH_BYTECODE (HTTPStream, "com/rmsl/juce/JuceHTTPStream", 16, javaJuceHttpStream)
 #undef JNI_CLASS_MEMBERS
 
 //==============================================================================

modules/juce_core/native/juce_mac_ObjCHelpers.h
@@ -342,15 +342,6 @@ namespace detail
     {
         return joinCompileTimeStr (v, makeCompileTimeStr (others...));
     }
-
-    template <typename Functor, typename Return, typename... Args>
-    static constexpr auto toFnPtr (Functor functor, Return (Functor::*) (Args...) const)
-    {
-        return static_cast<Return (*) (Args...)> (functor);
-    }
-
-    template <typename Functor>
-    static constexpr auto toFnPtr (Functor functor) { return toFnPtr (functor, &Functor::operator()); }
 } // namespace detail
 
 //==============================================================================
@@ -396,7 +387,7 @@ struct ObjCClass
     }
 
     template <typename Fn>
-    void addMethod (SEL selector, Fn callbackFn) { addMethod (selector, detail::toFnPtr (callbackFn)); }
+    void addMethod (SEL selector, Fn callbackFn) { addMethod (selector, toFnPtr (callbackFn)); }
 
     template <typename Result, typename... Args>
     void addMethod (SEL selector, Result (*callbackFn) (id, SEL, Args...))

modules/juce_gui_basics/juce_gui_basics.cpp
@@ -336,6 +336,28 @@ namespace juce
  #include "native/juce_linux_FileChooser.cpp"
 
 #elif JUCE_ANDROID
+
+namespace juce
+{
+static jobject makeAndroidRect (Rectangle<int> r)
+{
+    return getEnv()->NewObject (AndroidRect,
+                                AndroidRect.constructor,
+                                r.getX(),
+                                r.getY(),
+                                r.getRight(),
+                                r.getBottom());
+}
+
+static jobject makeAndroidPoint (Point<int> p)
+{
+    return getEnv()->NewObject (AndroidPoint,
+                                AndroidPoint.create,
+                                p.getX(),
+                                p.getY());
+}
+} // namespace juce
+
  #include "juce_core/files/juce_common_MimeTypes.h"
  #include "native/accessibility/juce_android_Accessibility.cpp"
  #include "native/juce_android_Windowing.cpp"

commit 4dfeb9de4d704ecdd8cc04c4a7874cb08cd462dc

Android: Fix juce_product_unlocking
Fixing a regression introduced in da38c1ed.

modules/juce_product_unlocking/native/juce_android_InAppPurchases.cpp
@@ -1012,32 +1012,32 @@ private:
         return responseCode == 0;
     }
 
-    void purchaseCompleted (jobject purchase, int responseCode)
+    static void purchaseCompleted (JNIEnv*, Pimpl& t, jobject purchase, int responseCode)
     {
-        notifyListenersAboutPurchase (buildPurchase (LocalRef<jobject> { purchase }),
-                                      wasSuccessful (responseCode),
-                                      getStatusDescriptionFromResponseCode (responseCode));
+        t.notifyListenersAboutPurchase (buildPurchase (LocalRef<jobject> { purchase }),
+                                        wasSuccessful (responseCode),
+                                        getStatusDescriptionFromResponseCode (responseCode));
     }
 
-    void purchaseConsumed (jstring productIdentifier, int responseCode)
+    static void purchaseConsumed (JNIEnv*, Pimpl& t, jstring productIdentifier, int responseCode)
     {
-        notifyListenersAboutConsume (juceString (LocalRef<jstring> { productIdentifier }),
-                                     wasSuccessful (responseCode),
-                                     getStatusDescriptionFromResponseCode (responseCode));
+        t.notifyListenersAboutConsume (juceString (LocalRef<jstring> { productIdentifier }),
+                                       wasSuccessful (responseCode),
+                                       getStatusDescriptionFromResponseCode (responseCode));
     }
 
-    void updateProductDetails (jobject productDetailsList)
+    static void updateProductDetails (JNIEnv*, Pimpl& t, jobject productDetailsList)
     {
-        jassert (! productDetailsQueryCallbackQueue.empty());
-        productDetailsQueryCallbackQueue.front() (LocalRef<jobject> { productDetailsList });
-        productDetailsQueryCallbackQueue.pop();
+        jassert (! t.productDetailsQueryCallbackQueue.empty());
+        t.productDetailsQueryCallbackQueue.front() (LocalRef<jobject> { productDetailsList });
+        t.productDetailsQueryCallbackQueue.pop();
     }
 
-    void updatePurchasesList (jobject purchasesList)
+    static void updatePurchasesList (JNIEnv*, Pimpl& t, jobject purchasesList)
     {
-        jassert (! purchasesListQueryCallbackQueue.empty());
-        purchasesListQueryCallbackQueue.front() (LocalRef<jobject> { purchasesList });
-        purchasesListQueryCallbackQueue.pop();
+        jassert (! t.purchasesListQueryCallbackQueue.empty());
+        t.purchasesListQueryCallbackQueue.front() (LocalRef<jobject> { purchasesList });
+        t.purchasesListQueryCallbackQueue.pop();
     }
 
     //==============================================================================