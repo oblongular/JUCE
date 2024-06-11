oblongular/JUCE

commit f76402662625856e94d59c125134de889b961227

ResizableWindow: Disable resizing border for desktop windows

modules/juce_gui_basics/windows/juce_ResizableWindow.cpp
@@ -254,6 +254,8 @@ void ResizableWindow::activeWindowStatusChanged()
 void ResizableWindow::setResizable (const bool shouldBeResizable,
                                     const bool useBottomRightCornerResizer)
 {
+    resizable = shouldBeResizable;
+
     if (shouldBeResizable)
     {
         if (useBottomRightCornerResizer)
@@ -271,7 +273,7 @@ void ResizableWindow::setResizable (const bool shouldBeResizable,
         {
             resizableCorner.reset();
 
-            if (resizableBorder == nullptr)
+            if (resizableBorder == nullptr && (! isOnDesktop() || ! Desktop::getInstance().supportsBorderlessNonClientResize()))
             {
                 resizableBorder.reset (new ResizableBorderComponent (this, constrainer));
                 Component::addChildComponent (resizableBorder.get());
@@ -293,8 +295,7 @@ void ResizableWindow::setResizable (const bool shouldBeResizable,
 
 bool ResizableWindow::isResizable() const noexcept
 {
-    return resizableCorner != nullptr
-        || resizableBorder != nullptr;
+    return resizable;
 }
 
 void ResizableWindow::setResizeLimits (int newMinimumWidth,

modules/juce_gui_basics/desktop/juce_Desktop.cpp
@@ -374,4 +374,13 @@ bool Desktop::isHeadless() const noexcept
     return displays->displays.isEmpty();
 }
 
+bool Desktop::supportsBorderlessNonClientResize() const
+{
+   #if JUCE_WINDOWS || JUCE_MAC
+    return true;
+   #else
+    return false;
+   #endif
+}
+
 } // namespace juce

modules/juce_gui_basics/desktop/juce_Desktop.h
@@ -412,6 +412,12 @@ public:
     static bool isOSXDarkModeActive()  { return Desktop::getInstance().isDarkModeActive(); }
    #endif
 
+    /** Returns true if the desktop environment allows resizing the window by clicking and dragging
+        just on/outside the window border.
+        MacOS and Windows 10+ both support this. Linux doesn't seem to. Mobile platforms do not.
+    */
+    bool supportsBorderlessNonClientResize() const;
+
     //==============================================================================
     /** Returns true on a headless system where there are no connected displays. */
     bool isHeadless() const noexcept;

modules/juce_gui_basics/windows/juce_ResizableWindow.h
@@ -401,7 +401,12 @@ protected:
 private:
     //==============================================================================
     Component::SafePointer<Component> contentComponent;
-    bool ownsContentComponent = false, resizeToFitContent = false, fullscreen = false, canDrag = true, dragStarted = false;
+    bool ownsContentComponent = false;
+    bool resizeToFitContent = false;
+    bool fullscreen = false;
+    bool canDrag = true;
+    bool dragStarted = false;
+    bool resizable = false;
     ComponentDragger dragger;
     Rectangle<int> lastNonFullScreenPos;
     ComponentBoundsConstrainer defaultConstrainer;

commit 1e7dae2e6f4567b59a32aab2ab231d5997a14497

ResizableWindow: Fix window becoming non-resizable when a constrainer is added
This is fixing a regression in f76402662625856e94d59c125134de889b961227.

modules/juce_gui_basics/windows/juce_ResizableWindow.cpp
@@ -326,13 +326,12 @@ void ResizableWindow::setConstrainer (ComponentBoundsConstrainer* newConstrainer
     {
         constrainer = newConstrainer;
 
-        bool useBottomRightCornerResizer = resizableCorner != nullptr;
-        bool shouldBeResizable = useBottomRightCornerResizer || resizableBorder != nullptr;
+        const bool useBottomRightCornerResizer = resizableCorner != nullptr;
 
         resizableCorner.reset();
         resizableBorder.reset();
 
-        setResizable (shouldBeResizable, useBottomRightCornerResizer);
+        setResizable (isResizable(), useBottomRightCornerResizer);
         updatePeerConstrainer();
     }
 }