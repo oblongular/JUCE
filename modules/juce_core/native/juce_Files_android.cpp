oblongular/JUCE

commit 0d2e34f34c6d75bbb10042fbac44a97b7f04393b

Typeface: Implement platform typefaces using Harfbuzz hb_font_t

modules/juce_core/native/juce_Files_android.cpp
@@ -584,14 +584,33 @@ struct AndroidStreamHelpers
 };
 
 //==============================================================================
-struct AndroidContentUriInputStream final :  public InputStream
+class AndroidInputStreamWrapper final : public InputStream
 {
-    explicit AndroidContentUriInputStream (const GlobalRef& uriIn)
-        : uri (uriIn),
-          stream (AndroidStreamHelpers::createStream (uri, AndroidStreamHelpers::StreamKind::input))
-    {}
+public:
+    explicit AndroidInputStreamWrapper (jobject streamIn)
+        : stream (LocalRef<jobject> { streamIn })
+    {
+    }
 
-    ~AndroidContentUriInputStream() override
+    AndroidInputStreamWrapper (AndroidInputStreamWrapper&& other) noexcept
+        : byteArray (std::exchange (other.byteArray, {})),
+          stream (std::exchange (other.stream, {})),
+          pos (std::exchange (other.pos, {})),
+          exhausted (std::exchange (other.exhausted, {}))
+    {
+    }
+
+    AndroidInputStreamWrapper (const AndroidInputStreamWrapper&) = delete;
+
+    AndroidInputStreamWrapper& operator= (AndroidInputStreamWrapper&& other) noexcept
+    {
+        std::swap (*this, other);
+        return *this;
+    }
+
+    AndroidInputStreamWrapper& operator= (const AndroidInputStreamWrapper&) = delete;
+
+    ~AndroidInputStreamWrapper() override
     {
         getEnv()->CallVoidMethod (stream.get(), AndroidInputStream.close);
         jniCheckHasExceptionOccurredAndClear();
@@ -625,24 +644,15 @@ struct AndroidContentUriInputStream final :  public InputStream
         return result;
     }
 
-    bool setPosition (int64 newPos) override
+    bool setPosition (int64) override
     {
-        if (newPos == pos)
-            return true;
-
-        if (pos < newPos)
-            return skipImpl (newPos - pos);
-
-        AndroidContentUriInputStream (uri).swap (*this);
-        return skipImpl (newPos);
+        return false;
     }
 
     int64 getPosition() override
     {
         return pos;
     }
-
-    bool openedSuccessfully() const { return stream != nullptr; }
 
     void skipNextBytes (int64 num) override
     {
@@ -664,19 +674,105 @@ private:
         return skipped == num;
     }
 
-    auto tie() { return std::tie (uri, byteArray, stream, pos, exhausted); }
-
-    void swap (AndroidContentUriInputStream& other) noexcept
-    {
-        auto toSwap = other.tie();
-        tie().swap (toSwap);
-    }
-
-    GlobalRef uri;
     CachedByteArray byteArray;
     GlobalRef stream;
     int64 pos = 0;
     bool exhausted = false;
+};
+
+std::unique_ptr<InputStream> makeAndroidInputStreamWrapper (jobject stream);
+std::unique_ptr<InputStream> makeAndroidInputStreamWrapper (jobject stream)
+{
+    return std::make_unique<AndroidInputStreamWrapper> (stream);
+}
+
+//==============================================================================
+struct AndroidContentUriInputStream final : public InputStream
+{
+    AndroidContentUriInputStream (AndroidContentUriInputStream&& other) noexcept
+        : stream (std::move (other.stream)),
+          uri (std::exchange (other.uri, {}))
+    {
+    }
+
+    AndroidContentUriInputStream (const AndroidContentUriInputStream&) = delete;
+
+    AndroidContentUriInputStream& operator= (AndroidContentUriInputStream&& other) noexcept
+    {
+        std::swap (*this, other);
+        return *this;
+    }
+
+    AndroidContentUriInputStream& operator= (const AndroidContentUriInputStream&) = delete;
+
+    int64 getTotalLength() override
+    {
+        return stream.getTotalLength();
+    }
+
+    bool isExhausted() override
+    {
+        return stream.isExhausted();
+    }
+
+    int read (void* destBuffer, int maxBytesToRead) override
+    {
+        return stream.read (destBuffer, maxBytesToRead);
+    }
+
+    bool setPosition (int64 newPos) override
+    {
+        if (newPos == getPosition())
+            return true;
+
+        if (getPosition() < newPos)
+            return skipImpl (newPos - getPosition());
+
+        auto newStream = fromUri (uri);
+
+        if (! newStream.has_value())
+            return false;
+
+        *this = std::move (*newStream);
+        return skipImpl (newPos);
+    }
+
+    int64 getPosition() override
+    {
+        return stream.getPosition();
+    }
+
+    void skipNextBytes (int64 num) override
+    {
+        stream.skipNextBytes (num);
+    }
+
+    static std::optional<AndroidContentUriInputStream> fromUri (const GlobalRef& uriIn)
+    {
+        const auto nativeStream = AndroidStreamHelpers::createStream (uriIn, AndroidStreamHelpers::StreamKind::input);
+
+        if (nativeStream == nullptr)
+            return {};
+
+        return AndroidContentUriInputStream { AndroidInputStreamWrapper { nativeStream }, uriIn };
+    }
+
+private:
+    AndroidContentUriInputStream (AndroidInputStreamWrapper streamIn, const GlobalRef& uriIn)
+        : stream (std::move (streamIn)),
+          uri (uriIn)
+    {
+    }
+
+    bool skipImpl (int64 num)
+    {
+        const auto oldPosition = getPosition();
+        skipNextBytes (num);
+        return getPosition() == oldPosition + num;
+    }
+
+    AndroidInputStreamWrapper stream;
+    GlobalRef uri;
 };
 
 //==============================================================================

examples/DemoRunner/Builds/Android/app/CMakeLists.txt
@@ -1814,8 +1814,6 @@ add_library( ${BINARY_NAME}
     "../../../../../modules/juce_graphics/fonts/harfbuzz/hb.hh"
     "../../../../../modules/juce_graphics/fonts/juce_AttributedString.cpp"
     "../../../../../modules/juce_graphics/fonts/juce_AttributedString.h"
-    "../../../../../modules/juce_graphics/fonts/juce_CustomTypeface.cpp"
-    "../../../../../modules/juce_graphics/fonts/juce_CustomTypeface.h"
     "../../../../../modules/juce_graphics/fonts/juce_Font.cpp"
     "../../../../../modules/juce_graphics/fonts/juce_Font.h"
     "../../../../../modules/juce_graphics/fonts/juce_FunctionPointerDestructor.h"
@@ -1826,6 +1824,7 @@ add_library( ${BINARY_NAME}
     "../../../../../modules/juce_graphics/fonts/juce_TextLayout.h"
     "../../../../../modules/juce_graphics/fonts/juce_Typeface.cpp"
     "../../../../../modules/juce_graphics/fonts/juce_Typeface.h"
+    "../../../../../modules/juce_graphics/fonts/juce_TypefaceTestData.cpp"
     "../../../../../modules/juce_graphics/geometry/juce_AffineTransform.cpp"
     "../../../../../modules/juce_graphics/geometry/juce_AffineTransform.h"
     "../../../../../modules/juce_graphics/geometry/juce_BorderSize.h"
@@ -1948,7 +1947,6 @@ add_library( ${BINARY_NAME}
     "../../../../../modules/juce_graphics/native/juce_Fonts_freetype.cpp"
     "../../../../../modules/juce_graphics/native/juce_Fonts_linux.cpp"
     "../../../../../modules/juce_graphics/native/juce_Fonts_mac.mm"
-    "../../../../../modules/juce_graphics/native/juce_Fonts_windows.cpp"
     "../../../../../modules/juce_graphics/native/juce_GraphicsContext_android.cpp"
     "../../../../../modules/juce_graphics/native/juce_IconHelpers_android.cpp"
     "../../../../../modules/juce_graphics/native/juce_IconHelpers_linux.cpp"
@@ -4278,8 +4276,6 @@ set_source_files_properties(
     "../../../../../modules/juce_graphics/fonts/harfbuzz/hb.hh"
     "../../../../../modules/juce_graphics/fonts/juce_AttributedString.cpp"
     "../../../../../modules/juce_graphics/fonts/juce_AttributedString.h"
-    "../../../../../modules/juce_graphics/fonts/juce_CustomTypeface.cpp"
-    "../../../../../modules/juce_graphics/fonts/juce_CustomTypeface.h"
     "../../../../../modules/juce_graphics/fonts/juce_Font.cpp"
     "../../../../../modules/juce_graphics/fonts/juce_Font.h"
     "../../../../../modules/juce_graphics/fonts/juce_FunctionPointerDestructor.h"
@@ -4290,6 +4286,7 @@ set_source_files_properties(
     "../../../../../modules/juce_graphics/fonts/juce_TextLayout.h"
     "../../../../../modules/juce_graphics/fonts/juce_Typeface.cpp"
     "../../../../../modules/juce_graphics/fonts/juce_Typeface.h"
+    "../../../../../modules/juce_graphics/fonts/juce_TypefaceTestData.cpp"
     "../../../../../modules/juce_graphics/geometry/juce_AffineTransform.cpp"
     "../../../../../modules/juce_graphics/geometry/juce_AffineTransform.h"
     "../../../../../modules/juce_graphics/geometry/juce_BorderSize.h"
@@ -4412,7 +4409,6 @@ set_source_files_properties(
     "../../../../../modules/juce_graphics/native/juce_Fonts_freetype.cpp"
     "../../../../../modules/juce_graphics/native/juce_Fonts_linux.cpp"
     "../../../../../modules/juce_graphics/native/juce_Fonts_mac.mm"
-    "../../../../../modules/juce_graphics/native/juce_Fonts_windows.cpp"
     "../../../../../modules/juce_graphics/native/juce_GraphicsContext_android.cpp"
     "../../../../../modules/juce_graphics/native/juce_IconHelpers_android.cpp"
     "../../../../../modules/juce_graphics/native/juce_IconHelpers_linux.cpp"

examples/DemoRunner/Builds/VisualStudio2017/DemoRunner_App.vcxproj
@@ -2096,9 +2096,6 @@
     <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_AttributedString.cpp">
       <ExcludedFromBuild>true</ExcludedFromBuild>
     </ClCompile>
-    <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_CustomTypeface.cpp">
-      <ExcludedFromBuild>true</ExcludedFromBuild>
-    </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_Font.cpp">
       <ExcludedFromBuild>true</ExcludedFromBuild>
     </ClCompile>
@@ -2109,6 +2106,9 @@
       <ExcludedFromBuild>true</ExcludedFromBuild>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_Typeface.cpp">
+      <ExcludedFromBuild>true</ExcludedFromBuild>
+    </ClCompile>
+    <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_TypefaceTestData.cpp">
       <ExcludedFromBuild>true</ExcludedFromBuild>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\geometry\juce_AffineTransform.cpp">
@@ -2349,9 +2349,6 @@
       <ExcludedFromBuild>true</ExcludedFromBuild>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\native\juce_Fonts_linux.cpp">
-      <ExcludedFromBuild>true</ExcludedFromBuild>
-    </ClCompile>
-    <ClCompile Include="..\..\..\..\modules\juce_graphics\native\juce_Fonts_windows.cpp">
       <ExcludedFromBuild>true</ExcludedFromBuild>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\native\juce_GraphicsContext_android.cpp">
@@ -4131,7 +4128,6 @@
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\harfbuzz\hb.h"/>
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\harfbuzz\hb.hh"/>
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_AttributedString.h"/>
-    <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_CustomTypeface.h"/>
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_Font.h"/>
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_FunctionPointerDestructor.h"/>
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_GlyphArrangement.h"/>

examples/DemoRunner/Builds/VisualStudio2017/DemoRunner_App.vcxproj.filters
@@ -2839,9 +2839,6 @@
     <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_AttributedString.cpp">
       <Filter>JUCE Modules\juce_graphics\fonts</Filter>
     </ClCompile>
-    <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_CustomTypeface.cpp">
-      <Filter>JUCE Modules\juce_graphics\fonts</Filter>
-    </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_Font.cpp">
       <Filter>JUCE Modules\juce_graphics\fonts</Filter>
     </ClCompile>
@@ -2852,6 +2849,9 @@
       <Filter>JUCE Modules\juce_graphics\fonts</Filter>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_Typeface.cpp">
+      <Filter>JUCE Modules\juce_graphics\fonts</Filter>
+    </ClCompile>
+    <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_TypefaceTestData.cpp">
       <Filter>JUCE Modules\juce_graphics\fonts</Filter>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\geometry\juce_AffineTransform.cpp">
@@ -3098,9 +3098,6 @@
       <Filter>JUCE Modules\juce_graphics\native</Filter>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\native\juce_Fonts_mac.mm">
-      <Filter>JUCE Modules\juce_graphics\native</Filter>
-    </ClCompile>
-    <ClCompile Include="..\..\..\..\modules\juce_graphics\native\juce_Fonts_windows.cpp">
       <Filter>JUCE Modules\juce_graphics\native</Filter>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\native\juce_GraphicsContext_android.cpp">
@@ -7123,9 +7120,6 @@
       <Filter>JUCE Modules\juce_graphics\fonts\harfbuzz</Filter>
     </ClInclude>
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_AttributedString.h">
-      <Filter>JUCE Modules\juce_graphics\fonts</Filter>
-    </ClInclude>
-    <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_CustomTypeface.h">
       <Filter>JUCE Modules\juce_graphics\fonts</Filter>
     </ClInclude>
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_Font.h">

examples/DemoRunner/Builds/VisualStudio2019/DemoRunner_App.vcxproj
@@ -2096,9 +2096,6 @@
     <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_AttributedString.cpp">
       <ExcludedFromBuild>true</ExcludedFromBuild>
     </ClCompile>
-    <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_CustomTypeface.cpp">
-      <ExcludedFromBuild>true</ExcludedFromBuild>
-    </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_Font.cpp">
       <ExcludedFromBuild>true</ExcludedFromBuild>
     </ClCompile>
@@ -2109,6 +2106,9 @@
       <ExcludedFromBuild>true</ExcludedFromBuild>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_Typeface.cpp">
+      <ExcludedFromBuild>true</ExcludedFromBuild>
+    </ClCompile>
+    <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_TypefaceTestData.cpp">
       <ExcludedFromBuild>true</ExcludedFromBuild>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\geometry\juce_AffineTransform.cpp">
@@ -2349,9 +2349,6 @@
       <ExcludedFromBuild>true</ExcludedFromBuild>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\native\juce_Fonts_linux.cpp">
-      <ExcludedFromBuild>true</ExcludedFromBuild>
-    </ClCompile>
-    <ClCompile Include="..\..\..\..\modules\juce_graphics\native\juce_Fonts_windows.cpp">
       <ExcludedFromBuild>true</ExcludedFromBuild>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\native\juce_GraphicsContext_android.cpp">
@@ -4131,7 +4128,6 @@
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\harfbuzz\hb.h"/>
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\harfbuzz\hb.hh"/>
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_AttributedString.h"/>
-    <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_CustomTypeface.h"/>
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_Font.h"/>
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_FunctionPointerDestructor.h"/>
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_GlyphArrangement.h"/>

examples/DemoRunner/Builds/VisualStudio2019/DemoRunner_App.vcxproj.filters
@@ -2839,9 +2839,6 @@
     <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_AttributedString.cpp">
       <Filter>JUCE Modules\juce_graphics\fonts</Filter>
     </ClCompile>
-    <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_CustomTypeface.cpp">
-      <Filter>JUCE Modules\juce_graphics\fonts</Filter>
-    </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_Font.cpp">
       <Filter>JUCE Modules\juce_graphics\fonts</Filter>
     </ClCompile>
@@ -2852,6 +2849,9 @@
       <Filter>JUCE Modules\juce_graphics\fonts</Filter>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_Typeface.cpp">
+      <Filter>JUCE Modules\juce_graphics\fonts</Filter>
+    </ClCompile>
+    <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_TypefaceTestData.cpp">
       <Filter>JUCE Modules\juce_graphics\fonts</Filter>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\geometry\juce_AffineTransform.cpp">
@@ -3098,9 +3098,6 @@
       <Filter>JUCE Modules\juce_graphics\native</Filter>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\native\juce_Fonts_mac.mm">
-      <Filter>JUCE Modules\juce_graphics\native</Filter>
-    </ClCompile>
-    <ClCompile Include="..\..\..\..\modules\juce_graphics\native\juce_Fonts_windows.cpp">
       <Filter>JUCE Modules\juce_graphics\native</Filter>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\native\juce_GraphicsContext_android.cpp">
@@ -7123,9 +7120,6 @@
       <Filter>JUCE Modules\juce_graphics\fonts\harfbuzz</Filter>
     </ClInclude>
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_AttributedString.h">
-      <Filter>JUCE Modules\juce_graphics\fonts</Filter>
-    </ClInclude>
-    <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_CustomTypeface.h">
       <Filter>JUCE Modules\juce_graphics\fonts</Filter>
     </ClInclude>
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_Font.h">

examples/DemoRunner/Builds/VisualStudio2022/DemoRunner_App.vcxproj
@@ -2096,9 +2096,6 @@
     <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_AttributedString.cpp">
       <ExcludedFromBuild>true</ExcludedFromBuild>
     </ClCompile>
-    <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_CustomTypeface.cpp">
-      <ExcludedFromBuild>true</ExcludedFromBuild>
-    </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_Font.cpp">
       <ExcludedFromBuild>true</ExcludedFromBuild>
     </ClCompile>
@@ -2109,6 +2106,9 @@
       <ExcludedFromBuild>true</ExcludedFromBuild>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_Typeface.cpp">
+      <ExcludedFromBuild>true</ExcludedFromBuild>
+    </ClCompile>
+    <ClCompile Include="..\..\..\..\modules\juce_graphics\fonts\juce_TypefaceTestData.cpp">
       <ExcludedFromBuild>true</ExcludedFromBuild>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\geometry\juce_AffineTransform.cpp">
@@ -2349,9 +2349,6 @@
       <ExcludedFromBuild>true</ExcludedFromBuild>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\native\juce_Fonts_linux.cpp">
-      <ExcludedFromBuild>true</ExcludedFromBuild>
-    </ClCompile>
-    <ClCompile Include="..\..\..\..\modules\juce_graphics\native\juce_Fonts_windows.cpp">
       <ExcludedFromBuild>true</ExcludedFromBuild>
     </ClCompile>
     <ClCompile Include="..\..\..\..\modules\juce_graphics\native\juce_GraphicsContext_android.cpp">
@@ -4131,7 +4128,6 @@
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\harfbuzz\hb.h"/>
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\harfbuzz\hb.hh"/>
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_AttributedString.h"/>
-    <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_CustomTypeface.h"/>
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_Font.h"/>
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_FunctionPointerDestructor.h"/>
     <ClInclude Include="..\..\..\..\modules\juce_graphics\fonts\juce_GlyphArrangement.h"/>

commit 67f06b4b084e57f3e69d2293bae1f6cccbd5b490

Android: Fix issues with input stream special members
Fixes bugs in AndroidInputStreamWrapper introduced in
0d2e34f34c6d75bbb10042fbac44a97b7f04393b

- Now that AndroidInputStreamWrapper is moveable, its destructor must be
  able to handle the situation where stream is null
- The move assignment operators of AndroidInputStreamWrapper and
  AndroidContentUriInputStream could previously end up calling
  themselves recursively

modules/juce_core/native/juce_Files_android.cpp
@@ -560,8 +560,8 @@ struct AndroidStreamHelpers
 class AndroidInputStreamWrapper final : public InputStream
 {
 public:
-    explicit AndroidInputStreamWrapper (jobject streamIn)
-        : stream (LocalRef<jobject> { streamIn })
+    explicit AndroidInputStreamWrapper (LocalRef<jobject> streamIn)
+        : stream (std::move (streamIn))
     {
     }
 
@@ -577,7 +577,7 @@ public:
 
     AndroidInputStreamWrapper& operator= (AndroidInputStreamWrapper&& other) noexcept
     {
-        std::swap (*this, other);
+        AndroidInputStreamWrapper { std::move (other) }.swap (*this);
         return *this;
     }
 
@@ -585,6 +585,9 @@ public:
 
     ~AndroidInputStreamWrapper() override
     {
+        if (stream == nullptr)
+            return;
+
         getEnv()->CallVoidMethod (stream.get(), AndroidInputStream.close);
         jniCheckHasExceptionOccurredAndClear();
     }
@@ -647,14 +650,22 @@ private:
         return skipped == num;
     }
 
+    void swap (AndroidInputStreamWrapper& other) noexcept
+    {
+        std::swap (other.byteArray, byteArray);
+        std::swap (other.stream, stream);
+        std::swap (other.pos, pos);
+        std::swap (other.exhausted, exhausted);
+    }
+
     CachedByteArray byteArray;
     GlobalRef stream;
     int64 pos = 0;
     bool exhausted = false;
 };
 
-std::unique_ptr<InputStream> makeAndroidInputStreamWrapper (jobject stream);
-std::unique_ptr<InputStream> makeAndroidInputStreamWrapper (jobject stream)
+std::unique_ptr<InputStream> makeAndroidInputStreamWrapper (LocalRef<jobject> stream);
+std::unique_ptr<InputStream> makeAndroidInputStreamWrapper (LocalRef<jobject> stream)
 {
     return std::make_unique<AndroidInputStreamWrapper> (stream);
 }
@@ -672,7 +683,7 @@ struct AndroidContentUriInputStream final : public InputStream
 
     AndroidContentUriInputStream& operator= (AndroidContentUriInputStream&& other) noexcept
     {
-        std::swap (*this, other);
+        AndroidContentUriInputStream { std::move (other) }.swap (*this);
         return *this;
     }
 
@@ -742,6 +753,12 @@ private:
         const auto oldPosition = getPosition();
         skipNextBytes (num);
         return getPosition() == oldPosition + num;
+    }
+
+    void swap (AndroidContentUriInputStream& other) noexcept
+    {
+        std::swap (other.stream, stream);
+        std::swap (other.uri, uri);
     }
 
     AndroidInputStreamWrapper stream;

modules/juce_graphics/native/juce_Fonts_android.cpp
@@ -93,7 +93,7 @@ DECLARE_JNI_CLASS (AndroidAssetManager, "android/content/res/AssetManager")
 #undef JNI_CLASS_MEMBERS
 
 // Defined in juce_core
-std::unique_ptr<InputStream> makeAndroidInputStreamWrapper (jobject stream);
+std::unique_ptr<InputStream> makeAndroidInputStreamWrapper (LocalRef<jobject> stream);
 
 struct AndroidCachedTypeface
 {