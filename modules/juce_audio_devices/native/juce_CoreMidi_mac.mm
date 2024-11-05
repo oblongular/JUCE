oblongular/JUCE

commit f7c7225f5c40a5aa04b3ff6bd6a426cc75f1bbb4

UIViewComponentPeer: Fix deprecation warnings for iOS 17

modules/juce_core/native/juce_ObjCHelpers_mac.h
@@ -593,6 +593,7 @@ private:
     }
 
 JUCE_DEFINE_IOS_VERSION_CHECKER_FOR_VERSION (14, 0)
+JUCE_DEFINE_IOS_VERSION_CHECKER_FOR_VERSION (17, 0)
 
 #endif
 

modules/juce_gui_basics/native/juce_UIViewComponentPeer_ios.mm
@@ -1019,16 +1019,26 @@ static bool doKeysUp (UIViewComponentPeer* owner, NSSet<UIPress*>* presses, UIPr
     return owner != nullptr && owner->canBecomeKeyWindow();
 }
 
-- (void) traitCollectionDidChange: (UITraitCollection*) previousTraitCollection
+static void postTraitChangeNotification (UITraitCollection* previousTraitCollection)
 {
-    [super traitCollectionDidChange: previousTraitCollection];
-
     const auto wasDarkModeActive = ([previousTraitCollection userInterfaceStyle] == UIUserInterfaceStyleDark);
 
     if (wasDarkModeActive != Desktop::getInstance().isDarkModeActive())
         [[NSNotificationCenter defaultCenter] postNotificationName: UIViewComponentPeer::getDarkModeNotificationName()
                                                             object: nil];
 }
+
+#if ! defined (__IPHONE_17_0) || __IPHONE_OS_VERSION_MAX_ALLOWED < __IPHONE_17_0
+- (void) traitCollectionDidChange: (UITraitCollection*) previousTraitCollection
+{
+    [super traitCollectionDidChange: previousTraitCollection];
+
+    if (@available (ios 17, *))
+        {} // do nothing
+    else
+        postTraitChangeNotification (previousTraitCollection);
+}
+#endif
 
 - (BOOL) isAccessibilityElement
 {
@@ -1671,6 +1681,23 @@ bool KeyPress::isKeyCurrentlyDown (int keyCode)
 
 Point<float> juce_lastMousePos;
 
+struct ChangeRegistrationTrait
+{
+   #if defined (__IPHONE_17_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_17_0
+    API_AVAILABLE (ios (17))
+    static void newFn (UIView* view)
+    {
+        [view registerForTraitChanges: @[UITraitUserInterfaceStyle.self]
+                          withHandler: ^(JuceUIView*, UITraitCollection* previousTraitCollection)
+        {
+            postTraitChangeNotification (previousTraitCollection);
+        }];
+    }
+   #endif
+
+    static void oldFn (UIView*) {}
+};
+
 //==============================================================================
 UIViewComponentPeer::UIViewComponentPeer (Component& comp, int windowStyleFlags, UIView* viewToAttachTo)
     : ComponentPeer (comp, windowStyleFlags),
@@ -1696,6 +1723,8 @@ UIViewComponentPeer::UIViewComponentPeer (Component& comp, int windowStyleFlags,
 
     if ((windowStyleFlags & ComponentPeer::windowRequiresSynchronousCoreGraphicsRendering) == 0)
         [[view layer] setDrawsAsynchronously: YES];
+
+    ifelse_17_0<ChangeRegistrationTrait> (view);
 
     if (isSharedWindow)
     {

commit 783be0e177af3c477fd2a46c43d2dc371a229dff

Compatibility: Add new macros for straightforward checking of minimum deployment target
This also fixes a bug introduced in f7c7225f5c40a5aa04b3ff6bd6a426cc75f1bbb4
where the condition guarding the definition of traitCollectionDidChange
was incorrect. This function is never required if the deployment target
is at least 17.0.

modules/juce_audio_devices/native/juce_CoreMidi_mac.mm
@@ -63,7 +63,7 @@ namespace CoreMidiHelpers
         onlyOld
     };
 
-    #if (MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_VERSION_11_0 || __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_14_0)
+    #if JUCE_MAC_API_VERSION_MIN_REQUIRED_AT_LEAST (11, 0) || JUCE_IOS_API_VERSION_MIN_REQUIRED_AT_LEAST (14, 0)
      #define JUCE_HAS_OLD_COREMIDI_API 0
      constexpr auto implementationStrategy = ImplementationStrategy::onlyNew;
     #else

modules/juce_core/native/juce_ObjCHelpers_mac.h
@@ -564,14 +564,6 @@ private:
     Class klass = nullptr;
 };
 
-// Expands to true if the API of the specified version is available at build time, false otherwise
-#define JUCE_MAC_API_VERSION_CAN_BE_BUILT(major, minor) \
-    major * 10'000 + minor * 100 <= MAC_OS_X_VERSION_MAX_ALLOWED
-
-// Expands to true if the API of the specified version is available at build time, false otherwise
-#define JUCE_IOS_API_VERSION_CAN_BE_BUILT(major, minor) \
-    major * 10'000 + minor * 100 <= __IPHONE_OS_VERSION_MAX_ALLOWED
-
 #if JUCE_IOS
 
 // Defines a function that will check the requested version both at

modules/juce_core/system/juce_TargetPlatform.h
@@ -132,7 +132,23 @@
 //==============================================================================
 #if JUCE_MAC || JUCE_IOS
 
-  #if defined (DEBUG) || defined (_DEBUG) || ! (defined (NDEBUG) || defined (_NDEBUG))
+// Expands to true if the API of the specified version is available at build time, false otherwise
+#define JUCE_MAC_API_VERSION_CAN_BE_BUILT(major, minor) \
+    ((major) * 10000 + (minor) * 100 <= MAC_OS_X_VERSION_MAX_ALLOWED)
+
+// Expands to true if the API of the specified version is available at build time, false otherwise
+#define JUCE_IOS_API_VERSION_CAN_BE_BUILT(major, minor) \
+    ((major) * 10000 + (minor) * 100 <= __IPHONE_OS_VERSION_MAX_ALLOWED)
+
+// Expands to true if the deployment target is greater or equal to the specified version, false otherwise
+#define JUCE_MAC_API_VERSION_MIN_REQUIRED_AT_LEAST(major, minor) \
+    ((major) * 10000 + (minor) * 100 <= MAC_OS_X_VERSION_MIN_REQUIRED)
+
+// Expands to true if the deployment target is greater or equal to the specified version, false otherwise
+#define JUCE_IOS_API_VERSION_MIN_REQUIRED_AT_LEAST(major, minor) \
+    ((major) * 10000 + (minor) * 100 <= __IPHONE_OS_VERSION_MIN_REQUIRED)
+
+#if defined (DEBUG) || defined (_DEBUG) || ! (defined (NDEBUG) || defined (_NDEBUG))
     #define JUCE_DEBUG 1
   #endif
 
@@ -161,9 +177,9 @@
   #endif
 
   #if JUCE_MAC
-    #if ! defined (MAC_OS_VERSION_11_1)
+    #if ! JUCE_MAC_API_VERSION_CAN_BE_BUILT (11, 1)
       #error "The macOS 11.1 SDK (Xcode 12.4+) is required to build JUCE apps. You can create apps that run on macOS 10.11+ by changing the deployment target."
-    #elif MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_11
+    #elif ! JUCE_MAC_API_VERSION_MIN_REQUIRED_AT_LEAST (10, 11)
       #error "Building for OSX 10.10 and earlier is no longer supported!"
     #endif
   #endif

modules/juce_gui_basics/juce_gui_basics.cpp
@@ -70,7 +70,7 @@
  #import <IOKit/pwr_mgt/IOPMLib.h>
  #import <MetalKit/MetalKit.h>
 
- #if defined (MAC_OS_VERSION_14_4) && MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_VERSION_14_4
+ #if JUCE_MAC_API_VERSION_MIN_REQUIRED_AT_LEAST (14, 4)
   #import <ScreenCaptureKit/ScreenCaptureKit.h>
  #endif
 

modules/juce_gui_basics/native/juce_UIViewComponentPeer_ios.mm
@@ -1028,7 +1028,7 @@ static void postTraitChangeNotification (UITraitCollection* previousTraitCollect
                                                             object: nil];
 }
 
-#if JUCE_IOS_API_VERSION_CAN_BE_BUILT (17, 0)
+#if ! JUCE_IOS_API_VERSION_MIN_REQUIRED_AT_LEAST (17, 0)
 - (void) traitCollectionDidChange: (UITraitCollection*) previousTraitCollection
 {
     [super traitCollectionDidChange: previousTraitCollection];

modules/juce_gui_basics/native/juce_Windowing_mac.mm
@@ -531,7 +531,7 @@ static Image createNSWindowSnapshot (NSWindow* nsWindow)
             return image;
         };
 
-       #if defined (MAC_OS_VERSION_14_4) && MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_VERSION_14_4
+       #if JUCE_MAC_API_VERSION_MIN_REQUIRED_AT_LEAST (14, 4)
 
         if (dlopen ("/System/Library/Frameworks/ScreenCaptureKit.framework/ScreenCaptureKit", RTLD_LAZY) == nullptr)
         {

modules/juce_opengl/opengl/juce_OpenGLContext.cpp
@@ -1198,9 +1198,11 @@ private:
 
         auto& comp = *getComponent();
 
-       #if JUCE_MAC && (! defined (MAC_OS_VERSION_15_0) || MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_VERSION_15_0)
+       #if JUCE_MAC
+        #if ! JUCE_MAC_API_VERSION_MIN_REQUIRED_AT_LEAST (15, 0)
         // According to a warning triggered on macOS 15 and above this doesn't do anything!
         [[(NSView*) comp.getWindowHandle() window] disableScreenUpdatesUntilFlush];
+        #endif
        #endif
 
         if (auto* oldCachedImage = CachedImage::get (comp))