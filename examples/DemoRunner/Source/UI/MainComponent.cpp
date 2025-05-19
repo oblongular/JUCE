oblongular/JUCE

commit 19061e6d17e21c7bf493644413238df6366521bc

Direct2D: Add initial support

examples/DemoRunner/Source/UI/MainComponent.cpp
@@ -288,7 +288,7 @@ MainComponent::MainComponent()
         {
            #if JUCE_MAC && USE_COREGRAPHICS_RENDERING
             setRenderingEngine (1);
-           #else
+           #elif ! JUCE_WINDOWS
             setRenderingEngine (0);
            #endif
         }

commit 8c57b80ada3279d4ab554ac80c3aff7eb06de5b6

DemoRunner: Avoid attempting to nest OpenGL contexts
OpenGL contexts normally can't be nested. Previously, when the main
renderer for the DemoRunner was set to OpenGL, we would forcibly reset
the engine to the software renderer when displaying any heavyweight
demo, including the OpenGL demos.

19061e6d introduced a regression, where the rendering engine was no
longer reset on Windows, so displaying an OpenGL demo while the global
renderer was also set to OpenGL would cause an assertion to fire in the
OpenGL Graphics context, after which rendering would fail.

With this change in place, we now fall back to the Direct2D renderer,
instead of the software renderer, when displaying a heavyweight demo.

examples/DemoRunner/Source/UI/MainComponent.cpp
@@ -286,11 +286,13 @@ MainComponent::MainComponent()
 
         if (isHeavyweight)
         {
-           #if JUCE_MAC && USE_COREGRAPHICS_RENDERING
-            setRenderingEngine (1);
-           #elif ! JUCE_WINDOWS
-            setRenderingEngine (0);
+           #if (JUCE_MAC && USE_COREGRAPHICS_RENDERING) || JUCE_WINDOWS
+            constexpr auto fallbackEngine = 1;
+           #else
+            constexpr auto fallbackEngine = 0;
            #endif
+
+            setRenderingEngine (fallbackEngine);
         }
 
         isShowingHeavyweightDemo = isHeavyweight;